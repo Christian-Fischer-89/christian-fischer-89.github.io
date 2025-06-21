3021ddedec38c1ab473f078cdb4882d8a164d877
Christian <christian.fischer89@web.de>
2025-06-22 01:20:03 +0200

    implemented logic of loading sections

---
+++ script.js
@@ -0,0 +1,47 @@
+//load sections
+document.addEventListener("DOMContentLoaded", async function () {
+    const sections_data = await fetch("sections/sections.txt");
+    const count = await sections_data.text();
+    for (let i = 0; i <= count; i++) {
+        var section = document.createElement("section");
+        section.id = `section-${i}`;
+        section.className = "container";
+        document.querySelector("main").append(section);
+    }
+    loadSections();
+});
+
+//load sections headings and texts 
+function loadSections() {
+    document.querySelectorAll("section").forEach(async text => {
+        var sectionPos = text.id.substring(text.id.indexOf("-") + 1);
+        var textData = await fetch("sections/" + sectionPos + ".txt");
+        var loadText = await textData.text();
+        var section_content = `<h2 id="section-${sectionPos}-heading">${loadText.slice(0, loadText.indexOf("\n"))}</h2>` + `<p id="section-${sectionPos}-text">${loadText.slice(loadText.indexOf("\n") + 1, loadText.length)}</p>`;
+        text.innerHTML = section_content + `<img id="pre-image-${sectionPos}" class="pre-images">`;
+        tryImages(document.getElementById(`pre-image-${sectionPos}`), `sections/images/${sectionPos}`);
+    });
+}
+
+//try images
+function tryImages(img, path) {
+    img.src = path + ".jpg";
+    img.onerror = function() {
+        img.src = path + ".JPG";
+        img.onerror = function() {
+            img.src = path + ".jpeg";
+            img.onerror = function() {
+                img.src = path + ".JPEG";
+                img.onerror = function() {
+                    img.src = path + ".png";
+                    img.onerror = function() {
+                        img.src = path + ".PNG";
+                        img.onerror = function() {
+                            img.parentNode.removeChild(img);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
